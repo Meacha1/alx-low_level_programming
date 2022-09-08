8be9a96ca00c150af1817b202234a5b4800c1553
Meacha1 <meachattd@gmail.com>
2022-09-08 11:19:33 -0700

    1st

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+        char stri[]="Programming is like building a multilingual puzzle";
+        puts(stri);
+        return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int v;
+    long int w;
+    long long int x;
+    char y;
+    float z;
+    printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(y));
+    printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(v));
+    printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(w));
+    printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(x));
+    printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(z));
+    return 0;
+}
