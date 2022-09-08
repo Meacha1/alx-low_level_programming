b6ba000bb85246774d6a1522f24c6ba56af8e980
Meacha1 <meachattd@gmail.com>
2022-09-08 16:16:55 -0700

    7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char stri[] = "\"Programming is like building a multilingual puzzle";
-		
+
 	puts(stri);
 	return (0);
 }
