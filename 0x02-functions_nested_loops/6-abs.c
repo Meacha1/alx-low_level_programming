c604b6166c8cf83c259ea80fd053fad0ed4e1624
Meacha1 <meachattd@gmail.com>
2022-09-13 09:45:34 -0700

    20

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _abs - check the code.
+*@n: The number to be checked
+*
+* Return: Always 0.
+*/
+int _abs(int n)
+{
+int n;
+
+if (n < 0)
+{
+int result;
+
+result = n * -1;
+
+return (result);
+}
+return (0);
+}
+
