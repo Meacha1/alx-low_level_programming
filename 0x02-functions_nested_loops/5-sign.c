91f0d5a1da3ba9247aed1ec29e1d402feaed084a
Meacha1 <meachattd@gmail.com>
2022-09-13 08:49:30 -0700

    19

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * print_sign - check the code.
- *@n: The number to be checked
- *
- * Return: Always 1 if n is +ve, 0 if 0 and -1 of -ve.
- */
+* print_sign - check the code.
+*@n: The number to be checked
+*
+* Return: Always 1 if n is +ve, 0 if 0 and -1 of -ve.
+*/
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                return (1);
-		_putchar('+');
-        }
-	else if (n = 0)
-        {
-                return (0);
-                _putchar('0');
-        }
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
 	else if (n < 0)
-        {
-                return (-1);
-                _putchar('-');
-        }
-
-return (0);
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
-
