48d36d9eec9b2e240ed6481d650cfc70c5b7c9a7
Meacha1 <meachattd@gmail.com>
2022-09-13 11:35:27 -0700

    26

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,23 +7,23 @@
   */
 void jack_bauer(void)
 {
-int s, t, u, v;
+	int a, b, c, d;
 
-	for (s = 0; s <= 2; s++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (t = 0; t <= 9; t++)
+		for (b = 0; b <= 9; b++)
 		{
-			if ((s <= 1 && t <= 9) || (s <= 2 && t <= 3))
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (u = 0; u <= 5; u++)
+				for (c = 0; c <= 5; c++)
 				{
-					for (v = 0; v <= 9; v++)
+					for (d = 0; d <= 9; d++)
 					{
-						_putchar(v + '0');
-						_putchar(t + '0');
+						_putchar(a + '0');
+						_putchar(b + '0');
 						_putchar(58);
-						_putchar(u + '0');
-						_putchar(v + '0');
+						_putchar(c + '0');
+						_putchar(d + '0');
 						_putchar('\n');
 					}
 				}
