3f88cf04e4fb3283fef90a31a82c0937952e0e6a
Meacha1 <meachattd@gmail.com>
2022-09-13 07:36:01 -0700

    8

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int c=1;
+
+for ( c = 1; c <=10; c++)
+{
+for ( i = 'a'; i < 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+}
