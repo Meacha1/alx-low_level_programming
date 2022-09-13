49a9b3a490a953adbf5a5aa490f427684085f85a
Meacha1 <meachattd@gmail.com>
2022-09-13 10:01:36 -0700

    22

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - Computes the absolute value of an integer.
+*@n: The number to be checked
+*
+* Return: Absolute value of number or zero
+*/
+int print_last_digit(int n)
+{
+int last_dig;
+last_dig = n % 10;
+return (last_dig);
+}
+
