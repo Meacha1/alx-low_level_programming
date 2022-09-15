29198160948f9fcf42feeb8000bc3328270a0da0
Meacha1 <meachattd@gmail.com>
2022-09-15 12:11:54 -0700

    commit message

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,3 +12,8 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void positive_or_negative(int i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#include <stdio.h>
