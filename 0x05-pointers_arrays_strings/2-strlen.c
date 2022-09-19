017e00630c5f5150a70a1cd1b2edb6c76ce702a7
Meacha1 <meachattd@gmail.com>
2022-09-19 10:34:26 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *
+ * @s: string to be checked
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char str[];
+	*s = _strlen(str);
+	return (0);
+}
