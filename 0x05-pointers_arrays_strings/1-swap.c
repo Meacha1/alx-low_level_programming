18322fec9558060d1b44d1ab5c2d3a0acd7739a0
Meacha1 <meachattd@gmail.com>
2022-09-19 10:04:29 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap a & b.
+ *
+ *@a: integer to be swaped.
+ *@b: integer to be swaped.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
+	
