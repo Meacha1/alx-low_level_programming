8cd4469f7d43b0e53034b32014e06a97b27fdf32
Meacha1 <meachattd@gmail.com>
2022-09-30 15:09:13 -0700

    commit message

---
+++ 1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    char i;
+    for( i = 'a'; i <= 'z'; i++;)
+	    _putchar(i);
+    return (0);
+}
