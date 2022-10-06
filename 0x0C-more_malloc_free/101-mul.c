031aa731a833b0594cd5e12bace43d46fc533037
Meacha1 <meachattd@gmail.com>
2022-10-06 12:30:47 -0700

    commit message

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,11 +14,11 @@ int i, j;
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
-	for (i = 1; i < argc; i++)
+	for (i = 1; i <= argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 && argv[i][j] < 48)
+			if (argv[i][j] > 57  argv[i][j] < 48)
 			{  printf("Error\n");
 			exit(98); }
 		}
