cd3bf2997cbde62dc4cd2ac4e261379cf7c18a9e
Meacha1 <meachattd@gmail.com>
2022-10-06 12:55:49 -0700

    commit message

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,13 +17,9 @@ int i, j;
 	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
+		if (atoi(argv[i]) * 0 !=0 || atoi(argv[i]) * 0 != 0)
 			{  printf("Error\n");
 			exit(98); }
-		}
-
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
