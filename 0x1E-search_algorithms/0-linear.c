d72730291331edd5c100030ec806c80003fa7293
Meacha1 <meachattd@gmail.com>
2023-04-11 03:54:45 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,13 +11,15 @@
  */
 
 int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
-    for (size_t i = 0; i < size; i++) {
-        printf("Comparing array[%zu] = %d to %d\n", i, array[i], value);
+	int i;
+
+	if (array == NULL) {
+		return (-1);
+	}
+    for (i = 0; i < (int)size; i++) {
+        printf("Value checked array[%u] = [%d]\n", i, array[i]);
         if (array[i] == value) {
-            return i;
+            return (i);
         }
     }
     return -1;
