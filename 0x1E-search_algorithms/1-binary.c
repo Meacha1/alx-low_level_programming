15d9867a576cc7fad2aab6940e6c4119cb78d50a
Meacha1 <meachattd@gmail.com>
2023-04-11 04:55:31 -0700

    Binary_search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
+ * binary_search - searches for a value in an array of
  * integers using the Binary search algorithm
  *
  *
